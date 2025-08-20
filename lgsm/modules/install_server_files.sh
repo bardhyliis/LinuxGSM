3ebcf67f3d4bc762d042d7f43ad5595dc4cbc45b
bardhyliis <bardhylsllamniku@gmail.com>
2025-08-20 16:20:20 +0200

    bug fix

--- lgsm/modules/install_server_files.sh
+++ lgsm/modules/install_server_files.sh
@@ -241,7 +241,7 @@ fn_install_server_files() {
 	fn_clear_tmp
 }
 
-elif [ "${shortname}" == "mc" ] || [ "${shortname}" == "pmc" ] || [ "${shortname}" == "neoforge" ] || [ "${shortname}" == "quilt" ] || [ "${shortname}" == "fabric" ] || [ "${shortname}" == "purpur" ] || [ "${shortname}" == "spigot" ]; then
+if [ "${shortname}" == "mc" ] || [ "${shortname}" == "pmc" ] || [ "${shortname}" == "neoforge" ] || [ "${shortname}" == "quilt" ] || [ "${shortname}" == "fabric" ] || [ "${shortname}" == "purpur" ] || [ "${shortname}" == "spigot" ]; then
 	install_eula.sh
 fi
 
